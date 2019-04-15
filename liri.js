662e06e2f56f5e5ed375011fde8a24c88b0ae69b
Andrew H <ahennesen@gmail.com>
2019-04-15 15:14:18 -0400

    running npm i and all the require documentation

--- liri.js
+++ liri.js
@@ -1,4 +1,19 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require('moment');
+moment().format();
+
+
+
+
 // used to call the api keys
 var spotify = new Spotify(keys.spotify);
+
+
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
