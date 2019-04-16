d09583861cd2b513492cf51f46963861ed7262dd
Andrew H <ahennesen@gmail.com>
2019-04-15 23:20:40 -0400

    added selectChoice fnc and very general Psuedo code. Currently Spotify keys not working, again

--- liri.js
+++ liri.js
@@ -1,18 +1,45 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var axios = require("axios");
-var moment = require('moment');
-moment().format();
+const keys = require("./keys.js");
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const moment = require('moment');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+let input = '';
+let song = '';
+let movie = '';
+let band = '';
+
+function selectChoice() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'start',
+      message: 'What can I help you with today?',
+      choices: ['concert-this','spotify-this-song', 'movie-this', 'do-what-it-says']
+    },
+  ]).then(function(answer){
+    if (answer.start === 'do-what-it-says'){
+      getRandom();
+      return;
+    } else {
+      liri = answer.start;
+      inquirer.prompt([
+       { type: input,
+         name: userimput,
+         message: 'What would you like for me to look up?'
+       }
+      ]).then(function (response){
+        input = response.userimput;
+      })
+    }
+  });
+}
+
+// the rest of the code here is going to be going through the choices.
+// current my spotify keys don't appear to be working and I am to tired to think straight. Will continue work tommrow.
+selectChoice();
 
 
-
-// used to call the api keys
-// var spotify = new Spotify(keys.spotify);
-
-
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  }
-);
